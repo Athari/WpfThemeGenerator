20e0653f163d228a0992d558b54471c6d5ffe1c5
Athari <athari.p@gmail.com>
2014-07-20 23:14:04 +0400

    Refactored Button style (moved chrome into ButtonChrome style).

--- Alba.WpfThemeGenerator/Themes/Chrome.cs
+++ Alba.WpfThemeGenerator/Themes/Chrome.cs
@@ -4,8 +4,34 @@ namespace Alba.WpfThemeGenerator.Themes
 {
     public static class Chrome
     {
+        public static readonly DependencyProperty IsCheckedProperty = DependencyProperty.RegisterAttached(
+            "IsChecked", typeof(bool?), typeof(Chrome), new PropertyMetadata(false));
+
+        public static void SetIsChecked (DependencyObject element, bool? value)
+        {
+            element.SetValue(IsCheckedProperty, value);
+        }
+
+        public static bool? GetIsChecked (DependencyObject element)
+        {
+            return (bool?)element.GetValue(IsCheckedProperty);
+        }
+
+        public static readonly DependencyProperty IsDefaultedProperty = DependencyProperty.RegisterAttached(
+            "IsDefaulted", typeof(bool), typeof(Chrome), new PropertyMetadata(false));
+
+        public static void SetIsDefaulted (DependencyObject element, bool value)
+        {
+            element.SetValue(IsDefaultedProperty, value);
+        }
+
+        public static bool GetIsDefaulted (DependencyObject element)
+        {
+            return (bool)element.GetValue(IsDefaultedProperty);
+        }
+
         public static readonly DependencyProperty IsFocusedProperty = DependencyProperty.RegisterAttached(
-            "IsFocused", typeof(bool), typeof(Chrome));
+            "IsFocused", typeof(bool), typeof(Chrome), new PropertyMetadata(false));
 
         public static void SetIsFocused (DependencyObject element, bool value)
         {
@@ -18,7 +44,7 @@ namespace Alba.WpfThemeGenerator.Themes
         }
 
         public static readonly DependencyProperty IsHoverProperty = DependencyProperty.RegisterAttached(
-            "IsHover", typeof(bool), typeof(Chrome));
+            "IsHover", typeof(bool), typeof(Chrome), new PropertyMetadata(false));
 
         public static void SetIsHover (DependencyObject element, bool value)
         {
@@ -28,6 +54,19 @@ namespace Alba.WpfThemeGenerator.Themes
         public static bool GetIsHover (DependencyObject element)
         {
             return (bool)element.GetValue(IsHoverProperty);
+        }
+
+        public static readonly DependencyProperty IsPressedProperty = DependencyProperty.RegisterAttached(
+            "IsPressed", typeof(bool), typeof(Chrome), new PropertyMetadata(false));
+
+        public static void SetIsPressed (DependencyObject element, bool value)
+        {
+            element.SetValue(IsPressedProperty, value);
+        }
+
+        public static bool GetIsPressed (DependencyObject element)
+        {
+            return (bool)element.GetValue(IsPressedProperty);
         }
     }
 }
