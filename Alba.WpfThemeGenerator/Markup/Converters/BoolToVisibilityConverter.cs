90aec5bd906c15f73d2fd95964d6f86df0c1fb0d
Athari <athari.p@gmail.com>
2014-07-21 23:35:30 +0400

    Added BoolToVisibilityConverter.

---
+++ Alba.WpfThemeGenerator/Markup/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Alba.WpfThemeGenerator.Markup
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public Visibility TrueVisibility { get; set; }
+        public Visibility FalseVisibility { get; set; }
+
+        public BoolToVisibilityConverter (Visibility trueVisibility = Visibility.Visible, Visibility falseVisibility = Visibility.Collapsed)
+        {
+            TrueVisibility = trueVisibility;
+            FalseVisibility = falseVisibility;
+        }
+
+        public object Convert (object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+                return DependencyProperty.UnsetValue;
+            return (bool)value ? TrueVisibility : FalseVisibility;
+        }
+
+        public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Visibility))
+                return DependencyProperty.UnsetValue;
+            var visibility = (Visibility)value;
+            if (visibility == TrueVisibility)
+                return true;
+            else if (visibility == FalseVisibility)
+                return false;
+            else
+                return Binding.DoNothing;
+        }
+    }
+}
--- Alba.WpfThemeGenerator/Markup/Converters/Converters.cs
+++ Alba.WpfThemeGenerator/Markup/Converters/Converters.cs
@@ -1,9 +1,14 @@
-﻿using System.Windows.Data;
+﻿using System.Windows;
+using System.Windows.Data;
 
+// ReSharper disable RedundantArgumentDefaultValue
 namespace Alba.WpfThemeGenerator.Markup
 {
     public static class Converters
     {
         public static readonly IMultiValueConverter Or = new OrConverter();
+        public static readonly IValueConverter BoolToVisibleCollapsed = new BoolToVisibilityConverter(Visibility.Visible, Visibility.Collapsed);
+        public static readonly IValueConverter BoolToVisibleHidden = new BoolToVisibilityConverter(Visibility.Visible, Visibility.Hidden);
+        public static readonly IValueConverter BoolToCollapsedVisible = new BoolToVisibilityConverter(Visibility.Collapsed, Visibility.Visible);
     }
 }
