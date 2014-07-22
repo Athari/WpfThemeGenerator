477a434f8d1eaea4a916128d048f19245c71b52f
Athari <athari.p@gmail.com>
2014-07-22 04:23:31 +0400

    Added customizable selection style support for TreeView.

---
+++ Alba.WpfThemeGenerator/Themes/AeroAlt/Attached/AeroStyles.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Alba.WpfThemeGenerator.Themes.AeroAlt.Attached
+{
+    public static class AeroStyles
+    {
+        public static readonly DependencyProperty SelectionStyleProperty = DependencyProperty.RegisterAttached(
+            "SelectionStyle", typeof(Style), typeof(AeroStyles));
+
+        public static void SetSelectionStyle (DependencyObject element, Style value)
+        {
+            element.SetValue(SelectionStyleProperty, value);
+        }
+
+        public static Style GetSelectionStyle (DependencyObject element)
+        {
+            return (Style)element.GetValue(SelectionStyleProperty);
+        }
+    }
+}
